045a024cdc94440b1c911f9825eb5e9e7b299e1d
sektor <sektor@template.com>
2021-06-08 00:36:36 +0300

    OkDz

--- lesson2-3/script.js
+++ lesson2-3/script.js
@@ -4,15 +4,19 @@ let operate;
 let result;
 
 parone = prompt('Введите первый операнд');
-parone == +parone;
+
+parone = +parone;
+
 if(Number.isNaN(parone)){
     alert('Ошибка ввода');
-}else {
-    operate = prompt('Введите оператор');
-    partwo = prompt('Ввведите второй операнд');
-  
+} else {
+    operate = prompt('Введите оператор'); 
 };
-
+partwo = prompt('Ввведите второй операнд');
+partwo = +partwo;
+if(Number.isNaN(partwo)){
+  alert('Ошибка ввода');
+} else {};
 switch (operate) {
     case '+':
       result = +parone + +partwo;

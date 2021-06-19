c47837ffc0a9294cecc4d2d0d83fd81880b48df0
sektor <sektor@template.com>
2021-06-19 21:38:29 +0300

    Current

--- lesson4-6/script.js
+++ lesson4-6/script.js
@@ -1,21 +1,29 @@
 let userNum;
 let userEven;
-button.onclick = function() {
- userNum = prompt();
- userNum = +userNum;
- if (Number.isNaN(userNum)){
-  alert(' Введите корректное значение!' );
+button.onclick = function showProm(){
+ userNum = +prompt();
+if (Number.isNaN(userNum) || userNum === 0){
+  alert('Введите корректное значение!');
  }else{
-  alert (userNum % 2 === 0 ? ' Odd'  : ' Even' );
-  alert (Number.isInteger(userNum) ? 'Integer':'NoInteger');
-  for(let i = 2; i < userNum; i++){
-      if(userNum % i === 0) {
-        alert(' Nosimple' )
-        } else {
-        alert(' Simple' )
-        };
-      break
+  alert (`
+    
+       Check to simple: ${calcSimple()};\n
+       Check to Integer: ${calcInteger()}\n
+       Check to Even:   ${calcEven()}\n
+          `);
     };
-  }; 
-};
-
+  };
+let calcEven = () => {
+ return userNum % 2 === 0 ? 'Even' : 'Odd'  ;
+}
+let calcInteger = () => {
+  return Number.isInteger(userNum) ? 'Integer':'NoInteger';
+}
+let calcSimple = () => {
+  for(let i = 2; i < userNum; i++){
+    if (userNum % i === 0) {
+      return ('Nosimple'); 
+      };
+    };
+      return ('Simple' );
+    };
